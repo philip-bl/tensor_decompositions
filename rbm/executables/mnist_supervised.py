9856a10eaf5ee7abd93bef01a468470975a14a15
Philip B <me@crabman.me>
2019-11-20 17:11:10 +0300

    Add cuda-device option to rbm/mnist_supervised.py

--- rbm/executables/mnist_supervised.py
+++ rbm/executables/mnist_supervised.py
@@ -1,3 +1,4 @@
+import logging
 import os.path
 from tempfile import mkdtemp
 
@@ -8,15 +9,19 @@ from torch.optim import SGD
 from rbm.mnist import HomogenousBinaryMNIST, IMG_SIDE_LEN
 from rbm.rbm import HomogenousBinaryRBM
 
+logger = logging.getLogger(__name__)
 
 dataset_root = os.path.expanduser("/mnt/hdd_1tb/datasets/mnist/dataset_source")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+logger.info(f"device = {device}")
+
 train_dataset = HomogenousBinaryMNIST(dataset_root, train=True)
 hidden_num_vars = 12
 model = HomogenousBinaryRBM(
     train_dataset.data.shape[1],
     hidden_num_vars,
     dataset_for_visible_bias_init=train_dataset,
-)
+).to(device)
 
 # we can check whether log_conditional_visible_likelihood does utter nonsense
 # by loading an unsupervised trained model, making predictions by using it and
@@ -25,15 +30,14 @@ model = HomogenousBinaryRBM(
 # model.load_state_dict(torch.load(os.path.expanduser("~/archive/experiments/2019-11_rbm_binary_small_mnist/h=12_trainmll=-162.83.pth")))
 input_num_vars = IMG_SIDE_LEN ** 2
 
-print(f"ln(Z) = {model.log_normalization_constant()}")
 optimizer = SGD(model.parameters(), 1.0, weight_decay=1e-9)
 tb_dir = mkdtemp()
 print(f"tb_dir = {tb_dir}")
-mnist_all_possible_y = torch.eye(len(train_dataset.classes))
+mnist_all_possible_y = torch.eye(len(train_dataset.classes)).to(device)
+condition = train_dataset.data[:, :input_num_vars].to(device)
+what = train_dataset.data[:, input_num_vars:].to(device)
 with tb.SummaryWriter(tb_dir) as tb_writer:
     for iteration in range(300001):
-        condition = train_dataset.data[:, :input_num_vars]
-        what = train_dataset.data[:, input_num_vars:]
         train_mean_conditional_visible_ll = torch.mean(
             model.log_conditional_visible_likelihood(
                 what, condition, mnist_all_possible_y
